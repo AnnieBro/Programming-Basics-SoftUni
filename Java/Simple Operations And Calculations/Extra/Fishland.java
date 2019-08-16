bf05cc74caca2bde676d289c83cc46dec76dbb0d
Anelia Milanova <aneliq.milanova@gmail.com>
2019-08-16 09:04:25 +0300

    Translated names of fishies

--- Java/Simple Operations And Calculations/Extra/Fishland.java
+++ Java/Simple Operations And Calculations/Extra/Fishland.java
@@ -4,17 +4,17 @@ public class Fishland {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double skumriqPrice = Double.parseDouble(scanner.nextLine());
-        double tzatzaPrice = Double.parseDouble(scanner.nextLine());
-        double palamud = Double.parseDouble(scanner.nextLine());
-        double safrid = Double.parseDouble(scanner.nextLine());
+        double mackerelPrice = Double.parseDouble(scanner.nextLine());
+        double spratPrice = Double.parseDouble(scanner.nextLine());
+        double chamois = Double.parseDouble(scanner.nextLine());
+        double mackerel = Double.parseDouble(scanner.nextLine());
         int mussels = Integer.parseInt(scanner.nextLine());
 
-        double palamudPrice = skumriqPrice * 1.6;
+        double chamoisPrice = skumriqPrice * 1.6;
         double safridPrice = tzatzaPrice * 1.8;
         double musselsPrice = 7.50;
 
-        double total = palamudPrice * palamud + safridPrice * safrid + musselsPrice * mussels;
+        double total = chamoisPrice * chamois + mackerelPrice * mackerel + musselsPrice * mussels;
         System.out.printf("%.2f", total);
     }
 }
