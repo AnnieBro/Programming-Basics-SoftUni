9f0b487c0cde10ecef9d6f21285e8d55a7b927e6
Anelia Milanova <aneliq.milanova@gmail.com>
2019-09-15 20:03:02 +0300

    Uploaded exercises to C#

---
+++ C#/02. Simple Operations and Calculations/Exercise/Alcohol Market.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alcohol_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double whiskeyPrice = double.Parse(Console.ReadLine());
+            double beer = double.Parse(Console.ReadLine());
+            double wine = double.Parse(Console.ReadLine());
+            double rakiya = double.Parse(Console.ReadLine());
+            double whiskey = double.Parse(Console.ReadLine());
+
+            double rakiyaPrice = whiskeyPrice / 2;
+            double winePrice = rakiyaPrice * (1 - 0.4);
+            double beerPrice = rakiyaPrice * (1 - 0.8);
+
+            double total = whiskeyPrice * whiskey + rakiyaPrice * rakiya + winePrice * wine + beerPrice * beer;
+            Console.WriteLine($"{total:F2}");
+        }
+    }
+}
---
+++ C#/02. Simple Operations and Calculations/Exercise/Charity Campaign.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Charity_Campaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int bakers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakePrice = 45 * cakes;
+            double wafflePrice = 5.8 * waffles;
+            double pancakePrice = 3.2 * pancakes;
+            double total = (cakePrice + wafflePrice + pancakePrice) * bakers * days;
+
+            Console.WriteLine($"{total - total / 8:F2}");
+        }
+    }
+}
